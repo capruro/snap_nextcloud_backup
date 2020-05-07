2099cc0f675cde0796e85c3ce810775303c6b945
capruro <64908287+capruro@users.noreply.github.com>
2020-05-07 12:48:23 +0200

    Create snap_nextcloud_backup.sh
    Edit the 3 variables:
    SNAPDIR={/snap/nextcloud/current}
    DATADIR={/var/snap/nextcloud/common/nextcloud/data}
    DESTDIR={/path/of/backup/nextcloud}

---
+++ snap_nextcloud_backup.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# Backup for Nextcloud
+#
+## VARIABLES
+SNAPDIR={/snap/nextcloud/current}
+DATADIR={/var/snap/nextcloud/common/nextcloud/data}
+DESTDIR={/path/of/backup/nextcloud}
+
+## CONS
+NAME=$(uname -n)
+DATE=$(date +'%Y-%m-%d')
+
+## SUB
+info()
+{
+	echo "${NAME}|$(date +'%Y-%m-%d %H:%M:%S INFO: ')${@}" 1>&2
+} # end info
+
+info "Checking Backup destination"
+if [[ -d $DESTDIR ]]
+then
+        info "Destination exist"
+else
+        info "Creating destination folder"
+        mkdir -p $DESTDIR/snap
+        chmod -r 700 $DESTDIR
+fi
+
+info "Stopping Nextcloud"
+snap stop nextcloud
+
+# backup Nextcloud
+if [[ -d $SNAPDIR ]]
+then
+        cd $SNAPDIR
+        info "Backing up Snap config folder"
+        tar -zcvf $DESTDIR/snap/nextcloud.snap.backup-$DATE.tar.gz .
+else
+        info "Snap Directory doesn't exist, exiting..."
+        exit 1
+fi
+
+# backup Data
+if [[ -d $DATADIR ]]
+then
+        cd $DATADIR
+        info "Backing up Data folder"
+        rsync -Pour $DATADIR $DESTDIR
+else
+        info "Data Directory doesn't exist, exiting..."
+        exit 1
+fi
+
+info "Starting Nextcloud"
+snap start nextcloud
+info "Backup Completed"
